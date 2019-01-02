62df78887d55128fae45e53c567667ec9290d537
Maher <maher91syr@gmail.com>
2019-01-02 23:40:46 +0900

    repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,11 +1,11 @@
 
-function repeatString(string, times) {
-    if(times < 0) 
-      return "";
-    if(times === 1) 
-      return string;
-    else 
-      return string + repeatString(string, times - 1);
+var repeatString = function(string, times) {
+    if (times < 0) return 'Error';
+    var result = '';
+    for (var i = 0; i < times; i++) {
+        result += string
+      }
+    return result
   }
 
 module.exports = repeatString

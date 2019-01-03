1c5321bc625c098d81a099e788e178a92362ed9b
Maher <maher91syr@gmail.com>
2019-01-03 20:12:48 +0900

    ReverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var splitString = str.split(""); 
+    var reverseStr = splitString.reverse(); 
+    var joinStr = reverseStr.join("");
 }
+return joinStr; 
 
 module.exports = reverseString
